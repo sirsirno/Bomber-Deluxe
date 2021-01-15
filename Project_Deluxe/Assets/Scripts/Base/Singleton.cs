381e492f994abb8ff33b2206ec895fe8f804190d
Double_U_ <junhyup511@gmail.com>
2021-01-15 20:25:51 +0900

    싱글톤 추가함

---
+++ Project_Deluxe/Assets/Scripts/Base/Singleton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance = null;
+    public static T Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = (T)FindObjectOfType(typeof(T)) as T;
+                if(instance == null)
+                {
+                    instance = new GameObject(typeof(T).ToString(), typeof(T)).GetComponent<T>();
+                }
+            }
+                        
+            return instance;
+        }
+    }
+    private void OnApplicationQuit()
+    {
+        instance = null;
+    }
+}
