d7dfe23f001fcebb5279624d5086a448bb414b20
Double_U_ <junhyup511@gmail.com>
2021-01-16 00:04:17 +0900

    간단한 플레이어랑 점프 구현

---
+++ Project_Deluxe/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : Singleton<PlayerController>
+{
+    public AudioClip jumpAudio;
+    public AudioClip respawnAudio;
+    public AudioClip ouchAudio;
+
+    /// <summary>
+    /// 플레이어 이동속도
+    /// </summary>
+    public float moveSpeed = 5f;
+
+    /// <summary>
+    /// 플레이어 점프속도
+    /// </summary>
+    public float jumpSpeed = 5f;
+
+    public PlayerState state = PlayerState.Grounded;
+    public bool controlEnabled = true;
+
+    private SpriteRenderer spriteRenderer;
+    Rigidbody2D rb;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (state == PlayerState.Grounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse); //위방향으로 올라가게함
+            state = PlayerState.Jumping;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (controlEnabled)
+        {
+
+            if (Input.GetKey(KeyCode.LeftArrow))    //왼쪽화살표 입력시 실행함
+            {
+                Vector3 scale = transform.localScale;
+                scale.x = -Mathf.Abs(scale.x);
+                transform.localScale = scale;
+                transform.Translate(Vector3.left * moveSpeed);
+                spriteRenderer.flipX = false;
+            }
+
+            if (Input.GetKey(KeyCode.RightArrow) )    //오른쪽화살표 입력시 실행함
+            {
+                Vector3 scale = transform.localScale;
+                scale.x = -Mathf.Abs(scale.x);
+                transform.localScale = scale;
+                transform.Translate(Vector3.right * moveSpeed);
+                spriteRenderer.flipX = true;
+            }
+        }
+    }
+
+    public enum PlayerState
+    {
+        Grounded,
+        Jumping,
+        Dead,
+        Stop
+    }
+}
---
+++ Project_Deluxe/Assets/Scripts/PlayerMoveCheck.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveCheck : MonoBehaviour
+{
+    /// <summary>
+    /// 플레이어 양옆으로 최고 속도
+    /// </summary>
+    public float moveSpeed = 5f;
+
+    private GameObject realPlayer; //직접 보이는 플레이어 오브젝트
+
+    bool cantGoLeft = false;
+    bool cantGoRight = false;
+
+    void Awake()
+    {
+        realPlayer = transform.parent.gameObject;
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "JumpTrigger")
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                cantGoLeft = true;
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                cantGoRight = true;
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "JumpTrigger")
+        {
+            cantGoLeft = false;
+            cantGoRight = false;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))    //왼쪽화살표 입력시 실행함
+        {
+            Vector3 scale = transform.localScale;
+            scale.x = -Mathf.Abs(scale.x);
+            transform.localScale = scale;
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+
+            if (!cantGoLeft)
+            {
+                realPlayer.transform.position = new Vector3(realPlayer.transform.position.x + transform.localPosition.x, realPlayer.transform.position.y + transform.localPosition.y, realPlayer.transform.position.z + transform.localPosition.z);
+                transform.localPosition = new Vector3(0, 0, 0);
+            }
+           realPlayer.GetComponent<SpriteRenderer>().flipX = true;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))    //오른쪽화살표 입력시 실행함
+        {
+            Vector3 scale = transform.localScale;
+            scale.x = -Mathf.Abs(scale.x);
+            transform.localScale = scale;
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+
+            if (!cantGoRight)
+            {
+                realPlayer.transform.position = new Vector3(realPlayer.transform.position.x + transform.localPosition.x, realPlayer.transform.position.y + transform.localPosition.y, realPlayer.transform.position.z + transform.localPosition.z);
+                transform.localPosition = new Vector3(0, 0, 0);
+            }
+
+            realPlayer.GetComponent<SpriteRenderer>().flipX = false;
+        }
+    }
+}
