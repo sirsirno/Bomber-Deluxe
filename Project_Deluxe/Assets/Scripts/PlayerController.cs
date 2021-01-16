babd12a3f29d7c95425a5c37f8f47b0fd4f52d37
Double_U_ <junhyup511@gmail.com>
2021-01-16 14:44:13 +0900

    애니메이션 일부구현

--- Project_Deluxe/Assets/Scripts/PlayerController.cs
+++ Project_Deluxe/Assets/Scripts/PlayerController.cs
@@ -22,28 +22,34 @@ public class PlayerController : Singleton<PlayerController>
     public bool controlEnabled = true;
 
     private SpriteRenderer spriteRenderer;
+    private Animator animator;
     Rigidbody2D rb;
 
     void Awake()
     {
+        animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
-        if (state == PlayerState.Grounded && Input.GetKeyDown(KeyCode.Space))
+        if (controlEnabled)
         {
-            rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse); //위방향으로 올라가게함
-            state = PlayerState.Jumping;
+            if (state == PlayerState.Grounded && Input.GetKeyDown(KeyCode.Space))
+            {
+                rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse); //위방향으로 올라가게함
+                state = PlayerState.Jumping;
+            }
         }
+
+        AnimationUpdate();
     }
 
     void FixedUpdate()
     {
         if (controlEnabled)
         {
-
             if (Input.GetKey(KeyCode.LeftArrow))    //왼쪽화살표 입력시 실행함
             {
                 Vector3 scale = transform.localScale;
@@ -62,6 +68,60 @@ public class PlayerController : Singleton<PlayerController>
                 spriteRenderer.flipX = true;
             }
         }
+    }
+
+    private void AnimationUpdate()
+    {
+        // PlayerAnimation
+        // 0 -> Idle
+        // 1 -> Walk
+        // 2 -> Jumping
+        // 3 -> After Jump Wait
+        // 4 -> Death
+
+        if (state == PlayerState.Grounded)
+        {
+            if (Input.GetKey(KeyCode.LeftArrow) && animator.GetInteger("PlayerAnimation") == 0)
+            {
+                animator.Play("Player_Walk");
+                animator.SetInteger("PlayerAnimation", 1);
+            }
+
+            if (Input.GetKey(KeyCode.RightArrow) && animator.GetInteger("PlayerAnimation") == 0)
+            {
+                animator.Play("Player_Walk");
+                animator.SetInteger("PlayerAnimation", 1);
+            }
+
+            if (!Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow))
+            {
+                if (animator.GetInteger("PlayerAnimation") == 0 || animator.GetInteger("PlayerAnimation") == 1)
+                {
+                    animator.Play("Player_Idle");
+                    animator.SetInteger("PlayerAnimation", 0);
+                }
+            }
+
+            if (animator.GetInteger("PlayerAnimation") == 2)
+            {
+                animator.Play("Player_AfterJumpWait");
+                animator.SetInteger("PlayerAnimation", 3);
+            }
+        }
+
+        if (state == PlayerState.Jumping)
+        {
+            if (animator.GetInteger("PlayerAnimation") == 0 || animator.GetInteger("PlayerAnimation") == 1)
+            {
+                animator.Play("Player_InputJump");
+                animator.SetInteger("PlayerAnimation", 2);
+            }
+        }
+    }
+
+    private void AfterJumpWait()
+    {
+        animator.SetInteger("PlayerAnimation", 0);
     }
 
     public enum PlayerState
