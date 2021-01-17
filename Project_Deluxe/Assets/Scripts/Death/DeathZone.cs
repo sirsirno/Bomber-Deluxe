76a740f0a2d8d60f4e231e31404a43762c05b1eb
sirsirno <wahybzb24@gmail.com>
2021-01-17 19:15:03 +0900

    죽을때 화면암전되게 함 DeathZone 스크립트 이용

--- Project_Deluxe/Assets/Scripts/Death/DeathZone.cs
+++ Project_Deluxe/Assets/Scripts/Death/DeathZone.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class DeathZone : MonoBehaviour
 {
+    public GameObject canvas;
+    public float DeathPanelDelayTime=4f;
     private GroundCheck groundCheck = null;
     private GameObject player = null;
     private GameObject playerController = null;
@@ -16,16 +20,22 @@ public class DeathZone : MonoBehaviour
         GetComponent<BoxCollider2D>().size = GetComponent<SpriteRenderer>().size;
         GetComponent<SpriteRenderer>().enabled = false;
     }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "PlayerController")
         {
+            canvas.SetActive(true);
+            Invoke("DeathPanelFalse",DeathPanelDelayTime);
             player.GetComponent<Animator>().SetInteger("PlayerAnimation", 5);
             player.GetComponent<Animator>().Play("Player_FallenDeath");
             playerController.GetComponent<Rigidbody2D>().simulated = false;
             groundCheck.GetComponent<GroundCheck>().enabled = false;
             playerController.GetComponent<PlayerController>().state = PlayerController.PlayerState.Dead;
         }
+    }
+
+    private void DeathPanelFalse(){
+    canvas.SetActive(false);
+    return; 
     }
 }
