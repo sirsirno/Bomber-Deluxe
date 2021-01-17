40fd64c292111cc5f42bbc04f1082d7a8944b53a
nexpang <rlarudgur123@gmail.com>
2021-01-17 15:18:58 +0900

    푸쉬

--- Project_Deluxe/Assets/Scripts/LandCheck.cs
+++ Project_Deluxe/Assets/Scripts/LandCheck.cs
@@ -7,6 +7,12 @@ public class LandCheck : MonoBehaviour
     [SerializeField]
     private GameObject player = null;
 
+    private Rigidbody2D rigid = null;
+
+    private void Start()
+    {
+        rigid = player.GetComponent<Rigidbody2D>();    
+    }
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "JumpTrigger")
@@ -15,9 +21,16 @@ public class LandCheck : MonoBehaviour
             {
                 if (player.GetComponent<PlayerController>().state == PlayerController.PlayerState.Grounded)
                 {
-                    player.GetComponent<Animator>().Play("Player_AfterJumpWait");
-                    player.GetComponent<Animator>().SetInteger("PlayerAnimation", 3);
-                    Debug.Log("ㅎㅇ");
+                    if (rigid.velocity.y < -10f)
+                    {
+                        player.GetComponent<Animator>().Play("Player_AfterJumpWait");
+                        player.GetComponent<Animator>().SetInteger("PlayerAnimation", 3);
+                        Debug.Log("ㅎㅇ");
+                    }
+                    else
+                    {
+                        player.GetComponent<Animator>().SetInteger("PlayerAnimation", 0);
+                    }
                 }
             }
         }
