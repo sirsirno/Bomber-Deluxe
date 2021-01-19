67667591e35fa2122510e2c8ac12046d7e8e7452
sirsirno <wahybzb24@gmail.com>
2021-01-19 18:36:44 +0900

    씬빼고 다올림

--- Project_Deluxe/Assets/Scripts/GetCoin.cs
+++ Project_Deluxe/Assets/Scripts/GetCoin.cs
@@ -4,12 +4,36 @@ using UnityEngine;
 
 public class GetCoin : MonoBehaviour
 {
+    public ParticleSystem parEatEffect;
+    public ParticleSystem badEatEffect;
+    public float offEffectTime=2.5f;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "PlayerController")
         {
-            Destroy(gameObject);
-            GameManager.Instance.AccessSetCoin(GameManager.SETTYPE.ADD, 1);
+            if(gameObject.tag == "BadCoin"){
+            badEatEffect.gameObject.SetActive(true);
+            Invoke("OffBadEffect",offEffectTime);
+            gameObject.SetActive(false);
+            return;
+            }
+            parEatEffect.gameObject.SetActive(true);
+            Invoke("OffEffect",offEffectTime);
+            gameObject.SetActive(false);
+            Invoke("DeleteObject",offEffectTime);
         }
+    }
+    private void OffBadEffect(){
+        badEatEffect.gameObject.SetActive(false);
+        return;
+    }
+    private void OffEffect(){
+        parEatEffect.gameObject.SetActive(false);
+        
+        return;
+    }
+    private void DeleteObject(){
+            Destroy(gameObject);
+             GameManager.Instance.AccessSetCoin(GameManager.SETTYPE.ADD, 1);
     }
 }
